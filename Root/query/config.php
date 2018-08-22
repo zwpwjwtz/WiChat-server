514558116314e6131e1451c825090dc0870c29ac
zwpwjwtz <zwpwjwtz@126.com>
2018-08-22 13:02:29 +0800

    Support "port" field in DNS response; support Web server.

--- Root/query/config.php
+++ Root/query/config.php
@@ -4,7 +4,9 @@ define('QUERY_LEN',10);
 define('QUERY_TYPE_NONE',0);
 define('QUERY_TYPE_GET_ACC',1);
 define('QUERY_TYPE_GET_REC',2);
+define('QUERY_TYPE_GET_WEB',4);
 
-$AccServer=array('127.0.0.1');
-$RecServer=array('127.0.0.1');
+$AccServer=array(array('address'=>'127.0.0.1','port'=>'80'));
+$RecServer=array(array('address'=>'127.0.0.1','port'=>'80'));
+$WebServer=array(array('address'=>'127.0.0.1','port'=>'80'));
 ?>
--- Root/query/index.php
+++ Root/query/index.php
@@ -1,6 +1,18 @@
 <?php
 require_once('../common/config.php');
 require_once('./config.php');
+
+function serverListToString($serverList,$port=true)
+{
+	$result=chr(RESPONSE_SUCCESS).chr(count($serverList));
+	foreach ($serverList as $server)
+	{
+		$result.=$server['address'].chr(0);
+		if ($port) $result.=$server['port'].chr(0);
+	}
+	return $result;
+}
+
 if (!(defined('ROOT_SERVER') && defined('RESPONSE_HEADER') && isset($AccServer,$RecServer))) //Fatal Error
 {
 	exit(0);
@@ -16,29 +28,27 @@ else
 	if (substr($buffer,0,QUERY_HEADER_LEN)!=QUERY_HEADER) $out.=chr(RESPONSE_INVALID);
 	else
 	{
-		if (!in_array(ord(substr($buffer,QUERY_HEADER_LEN,1)),$validVersion)) $out.=chr(RESPONSE_DEVICE_UNSUPPORTED);
+		$version=ord(substr($buffer,QUERY_HEADER_LEN,1));
+		if (!in_array($version,$validVersion)) $out.=chr(RESPONSE_DEVICE_UNSUPPORTED);
 		else
 		{
 			$option=ord(substr($buffer,QUERY_HEADER_LEN+1,1));
+			$usePort=($version>5);
 			if ($option==QUERY_TYPE_NONE)
 				$out.=chr(RESPONSE_SUCCESS).chr(0);
 			else
 			{
-				$serverList='';
+				$outContent='';
 				if ($option & QUERY_TYPE_GET_ACC)
-				{
-					$serverList.=chr(RESPONSE_SUCCESS).chr(count($AccServer));
-					foreach ($AccServer as $item) $serverList.=$item.chr(0);
-				}
+					$outContent.=serverListToString($AccServer,$usePort);
 				if ($option & QUERY_TYPE_GET_REC) 
-				{
-					$serverList.=chr(RESPONSE_SUCCESS).chr(count($RecServer));
-					foreach ($RecServer as $item) $serverList.=$item.chr(0);
-				}
-				if ($serverList=='')
+					$outContent.=serverListToString($RecServer,$usePort);
+				if ($option & QUERY_TYPE_GET_WEB) 
+					$outContent.=serverListToString($WebServer,$usePort);
+				if ($outContent=='')
 					$out.=chr(RESPONSE_INVALID);
 				else
-					$out.=$serverList;
+					$out.=$outContent;
 			}
 		}
 	}
